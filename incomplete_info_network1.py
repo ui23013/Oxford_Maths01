0130fe735f028f28b1b28d3259a5ac125c87525f
ui23013 <160421575+ui23013@users.noreply.github.com>
2026-07-14 13:42:36 +0100

    Update with edge removal func

--- incomplete_info_network1.py
+++ incomplete_info_network1.py
@@ -37,19 +37,66 @@ def run_first_sim(graph, beta, gamma, rho, tau):
     # retrieve infected and recovered nodes at time tau
     infected_nodes = [node for node, status in first_states.items() if status == 'I']
 
-    recovered_nodes = [node for node, status in first_states.items() if status == 'R']
-    return infected_nodes, recovered_nodes
+    # don't know if this is needed
+    # recovered_nodes = [node for node, status in first_states.items() if status == 'R']
+
+
+    return infected_nodes
+
+
+def edge_removal(graph, p, q,  i_nodes):
+    total_edges = graph.number_of_edges()
+    removal_count = round(q*total_edges)
+
+    # from sam
+    randgen = random.Random()
+
+    # define set of all infected nodes
+    infected_nodes = set(i_nodes)
+
+    for e in range(removal_count):
+        current_edges = list(graph.edges)
+
+        # in case no. of removals exceeds no. of edges
+        if not current_edges:
+            break
+
+        # identify set of edges that are connected to at least 1 infected node
+        informed_edges = {edge for edge in current_edges
+                        if edge[0] in infected_nodes or edge[1] in infected_nodes}
+
+        # identify edges that arent connected to an infected node on either end
+        # i.e current edges - informed_edges
+        uninformed_edges = [edge for edge in current_edges
+                            if edge not in informed_edges]
+
+        # decide how many edges should be informatively removed and otherwise
+        informed_remove_count = round(p * removal_count)
+        uninformed_remove_count = removal_count - informed_remove_count
+
+        # and sample from their respective edge lists
+        removed_informed = random.sample(informed_edges, informed_remove_count)
+        removed_uninformed = random.sample(uninformed_edges, uninformed_remove_count)
+
+        # and remove them all
+        removed_edges = removed_informed + removed_uninformed
+        graph.removes_edge_from(*removed_edges)
+
+        # prefer an edge adjacent to an infected node
+        # if informed_edges and randgen.random() < p:
+            # removed_edge = randgen.choice(informed_edges)
+        # else:
+            # removed_edge = randgen.choice(uninformed_edges)
+
+        # graph.remove_edge(*remove_edge)
+
+
+
 
 def simulate_incomp_info_SIR(N, k, beta, gamma, rho, tau, p, q):
     '''
     Simulate the SIR model which models having incomplete information
     Args:
-        N: no. of nodes
-        k: degree of node
-        beta: transmission rate
-        gamma: recovery rate
-        rho: proportion of population initially infected
-        tau: info time
         p: probability of informed removal
         q: proportion of nodes to be removed
 
@@ -59,8 +106,8 @@ def simulate_incomp_info_SIR(N, k, beta, gamma, rho, tau, p, q):
     # create rrg graph
     sim_g = create_rrg(k, N)
 
-    # simulate until time tau and extrac
-    first_sim = eon.fast_SIR(sim_g, beta, gamma, rho=rho, tmax=tau, return_full_data=True)
+    # simulate until time tau and extract the infected nodes
+    infected_nodes = run_first_sim(sim_g, beta, gamma, rho, tau)
 
 
 
