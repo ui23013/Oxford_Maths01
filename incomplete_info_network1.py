04c0a2bc2bf506f3a7a17478f48f4fdaf3a27501
ui23013 <160421575+ui23013@users.noreply.github.com>
2026-07-15 16:08:16 +0100

    Update
    With base cases of no, random and perfect intervention which can be compared to one another to see how they perform

--- incomplete_info_network1.py
+++ incomplete_info_network1.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import EoN as eon
 import random
+import pandas as pd
 
 
 def create_rrg(k, N, seed):
@@ -77,17 +78,14 @@ def edge_removal(graph, p, q,  i_nodes):
 
         graph.remove_edge(*removed_edge)
 
+        # print('Informed edges:', len(informed_edges)) # debug
     return graph
 
 
-def run_second_sim(graph, beta, gamma, initial_states,tmin, tmax):
+def run_second_sim(graph, beta, gamma, infected_nodes, recovered_nodes,tmin, tmax):
 
-    # extract which nodes were infected/recovered at time tmin
-    I_nodes = [node for node, status in initial_states.items() if status == 'I']
-    R_nodes = [node for node, status in initial_states.items() if status == 'R']
-
-    sim = eon.Gillespie_SIR(graph, beta, gamma, initial_infecteds=I_nodes, initial_recovereds=R_nodes, tmin=tmin,
-                            tmax=tmax, return_full_data=True)
+    sim = eon.Gillespie_SIR(graph, beta, gamma, initial_infecteds=infected_nodes, initial_recovereds=recovered_nodes,
+                            tmin=tmin, tmax=tmax, return_full_data=True)
 
     return sim
 
@@ -104,12 +102,15 @@ def sim_single_intervention(N, k, beta, gamma, rho, tau, p, q, sim_duration, see
     # simulate until time tau and extract the infected nodes
     infected_nodes, recovered_nodes, first_states = run_first_sim(sim_g, beta, gamma, rho, tau)
 
+    # debug
+    # print('Infected nodes', len(infected_nodes))
+
     # obtain graph after edge removal
     modified_graph = edge_removal(sim_g, p, q, infected_nodes)
     edges_removed = initial_edges - modified_graph.number_of_edges()
 
     # and run a second sum to see results of intervention
-    second_sim = run_second_sim(modified_graph, beta, gamma, first_states, tmin=tau, tmax=sim_duration)
+    second_sim = run_second_sim(modified_graph, beta, gamma, infected_nodes, recovered_nodes, tmin=tau, tmax=sim_duration)
     final_states = second_sim.get_statuses(time=sim_duration) # states at final time
 
     # outcomes
@@ -117,22 +118,60 @@ def sim_single_intervention(N, k, beta, gamma, rho, tau, p, q, sim_duration, see
     final_recovered = sum(1 for status in final_states.values() if status == 'R')
     total_infected = final_infected + final_recovered
 
-    return modified_graph, edges_removed, final_infected, final_recovered, total_infected
+    return modified_graph, edges_removed, final_infected, final_recovered, total_infected, len(infected_nodes)
 
 
-# set graph seed for reproducibility
-graph_seed=random.seed(0)
+# # set graph seed for reproducibility
+# graph_seed = 120
+# no_intervention = sim_single_intervention(N=1000, k=3, beta=0.4, gamma=0.1, rho=0.05, tau=20, p=1, q=0, sim_duration=500
+#                                           , seed=graph_seed)
+# print('No intervention stats', no_intervention)
+#
+# # random intervention
+# test_run_p0= sim_single_intervention(N=1000, k=3, beta=0.4, gamma=0.1, rho=0.05, tau=20, p=0, q=0.1, sim_duration=500
+#                                      , seed=graph_seed)
+# print('Random intervention stats',test_run_p0)
+#
+# # perfect information
+# test_run_p1 = sim_single_intervention(N=1000, k=3, beta=0.4, gamma=0.1, rho=0.05, tau=20, p=1, q=0.1, sim_duration=500
+#                                       , seed=graph_seed)
+# print('Perfect intervention stats',test_run_p1)
 
-no_intervention = sim_single_intervention(N=1000, k=3, beta=0.4, gamma=0.1, rho=0.05, tau=20, p=1, q=0, sim_duration=100
-                                          , seed=graph_seed)
-print(no_intervention) # shows edges removed, final_infected = 0 and final_recovered, final_infected = n
 
-# perfect information
-test_run_p1 = sim_single_intervention(N=1000, k=3, beta=0.4, gamma=0.1, rho=0.05, tau=20, p=1, q=0.1, sim_duration=100
-                                      , seed=graph_seed)
-print(test_run_p1)
+def run_repeated_sims(N, k, beta, gamma, rho, tau, p, q, sim_duration, seed, n_runs=25):
+    records = []
+    for _ in range(n_runs):
+        (modified_graph, edges_removed, final_infected, final_recovered, total_infected, seed_infected) = (
+            sim_single_intervention(N=N, k=k, beta=beta, gamma=gamma, rho=rho, tau=tau, p=p, q=q,
+                                    sim_duration=sim_duration, seed=seed))
 
-# random intervention
-test_run_p0= sim_single_intervention(N=1000, k=3, beta=0.4, gamma=0.1, rho=0.05, tau=20, p=0, q=0.1, sim_duration=100
-                                     , seed=graph_seed)
-print(test_run_p0)
+        records.append({'seed_infected': seed_infected, 'edges_removed': edges_removed, 'final_infected':final_infected,
+                        'final_recovered': final_recovered, 'total_infected': total_infected,})
+
+    return pd.DataFrame(records)
+
+
+# run sim multiple times to get data
+# df = run_repeated_sims(N=1000, k=3, beta=0.4, gamma=0.1, rho=0.05, tau=20, p=1, q=0.1, sim_duration=500, seed=120,
+#                        n_runs=25)
+# df['seed_infected'].describe()
+# df['total_infected'].describe()
+
+def compare_interventions(N, k, beta, gamma, rho, tau, sim_duration, seed, n_runs):
+    df_no = run_repeated_sims(N=N, k=k, beta=beta, gamma=gamma, rho=rho, tau=tau,
+                               p=1, q=0.0, sim_duration=sim_duration, seed=seed, n_runs=n_runs)
+    df_random = run_repeated_sims(N=N, k=k, beta=beta, gamma=gamma, rho=rho, tau=tau,
+                                   p=0, q=0.1, sim_duration=sim_duration, seed=seed, n_runs=n_runs)
+    df_perfect = run_repeated_sims(N=N, k=k, beta=beta, gamma=gamma, rho=rho, tau=tau,
+                                    p=1, q=0.1, sim_duration=sim_duration, seed=seed, n_runs=n_runs)
+
+    return pd.Series({
+        'no_intervention': (df_no['final_recovered'] / N).mean(),
+        'random_intervention': (df_random['final_recovered'] / N).mean(),
+        'perfect_intervention': (df_perfect['final_recovered'] / N).mean(),
+        }, name='mean_r_inf')
+
+
+r_inf_summary = compare_interventions(N=1000, k=3, beta=0.5, gamma=0.3, rho=0.05, tau=10, sim_duration=1000, seed=120,
+                                      n_runs=100)
+print(r_inf_summary)
